d4d343ed1e6a7d86d5d06d257ad36489d72e86e4
BrunoMreis <brbrno@hotmail.com>
2026-01-31 16:21:09 -0300

    Correção vulnerabilidades CSRF

--- src/main/java/com/trugdz/frase_de_pinguim/controller/UserController.java
+++ src/main/java/com/trugdz/frase_de_pinguim/controller/UserController.java
@@ -1,16 +1,21 @@
 package com.trugdz.frase_de_pinguim.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.trugdz.frase_de_pinguim.dto.CreateUserDTO;
 import com.trugdz.frase_de_pinguim.dto.UserDetailsResponseDTO;
 import com.trugdz.frase_de_pinguim.dto.UserResponseDTO;
-import com.trugdz.frase_de_pinguim.model.Frase;
-import com.trugdz.frase_de_pinguim.model.User;
 import com.trugdz.frase_de_pinguim.service.UserService;
-import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("users")
@@ -27,8 +32,6 @@ public class UserController {
 
     @GetMapping("/{id}")
     public UserDetailsResponseDTO getById(@PathVariable Long id){ return userService.getById(id); }
-
-
 
     @PostMapping
     public UserResponseDTO create(@Valid @RequestBody CreateUserDTO request){ return userService.create(request); }
